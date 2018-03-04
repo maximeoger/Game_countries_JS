8c1af104b2dc8acd42c78cc2a8db3233f1663046
maximeoger <maximeoger93@gmail.com>
2018-03-04 16:48:51 +0100

    crated game over

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var start_btn = document.querySelector('.start');
 var to_find = document.querySelector('.to_find');
 var start_window = document.querySelector('#game-start');
+var game_over_window = document.querySelector('#game-over');
+var counter_disp = document.querySelector('.time span');
 
+/* set the time in seconds for the game */
+var time = 20;
 
-function gameLauncher() {
-    start_window.classList.toggle('is-open');
+function counter () {
+    timer = setTimeout(function (){
+        console.log('tic tac');
+    }, 1000 * time);
 }
+
+function counterStart () {
+    var counter = value;
+    counter_disp.innerHTML = value;
+    counter();
+}
+
+function gameLauncher () {
+    start_window.classList.toggle('is-open');
+    counterStart(); 
+}
+
+function gameOver () {
+    game_over_window.classList.toggle('is-open');
+}
+
+
 
 start_btn.addEventListener('click', gameLauncher);
