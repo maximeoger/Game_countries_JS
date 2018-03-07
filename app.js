837972c646ecc0cac3e7bcbd1652bcf3d52b4abe
maximeoger <maximeoger93@gmail.com>
2018-03-07 18:31:55 +0100

    commentaires

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var start_window = document.querySelector('#game-start');
 var game_over_window = document.querySelector('#game-over');
 var counter_disp = document.querySelector('.time span');
 
-/* set the time in seconds for the game */
+/* temps d'un Round en secondes ( modifiable selon la difficultée ) */
 var timeMax = 20;
 
+/* cette fonction gère le compteur, toutes les 1000 ms (1sec) il décrémente de 1 la valeur de timeMax */
 function counterStart (value) {
     var timeLeft = value;
     counter_disp.innerHTML = timeLeft;
@@ -25,15 +26,18 @@ function counterStart (value) {
     
 }
 
+/* affiche la fenetre de jeu et démarre le compteur ( fonction précédente ) */
 function gameLauncher () {
     start_window.classList.toggle('is-open');
     counterStart(timeMax); 
 }
 
+/* fonction qui lance la fenettre du game over (résultant de la fonction counterStart si timeMax arrive à zéro) */ 
 function gameOver () {
     game_over_window.classList.toggle('is-open');
 }
 
+/* lance le jeu */
 start_btn.addEventListener('click', gameLauncher);
 restart_btn.addEventListener('click', function(){
     game_over_window.classList.remove('is-open');
