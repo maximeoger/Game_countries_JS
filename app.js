0e866458c60b076158e2f5a8519733816b735eaf
LeKawa <hector.travaille@hetic.net>
2018-03-08 15:48:51 +0100

    problème

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+var start_btn = document.querySelector('.start');
+
+var restart_btn = document.querySelector('.restart');
+var to_find = document.querySelector('.to_find');
+var start_window = document.querySelector('#game-start');
+var game_over_window = document.querySelector('#game-over');
+var counter_disp = document.querySelector('.time span');
 var el = document.querySelectorAll('.img');
 var active_flags = [0, 1, 2, 3];
+
+
 
 
 function generateFlags() {
@@ -30,6 +39,46 @@ function generateFlags() {
 
 start_btn.addEventListener('click', generateFlags());
 
+/* temps d'un Round en secondes ( modifiable selon la difficultée ) */
+var timeMax = 20;
+
+/* cette fonction gère le compteur, toutes les 1000 ms (1sec) il décrémente de 1 la valeur de timeMax */
+function counterStart(value) {
+  var timeLeft = value;
+  counter_disp.innerHTML = timeLeft;
+
+  timer = setInterval(function() {
+
+    timeLeft--;
+    counter_disp.innerHTML = timeLeft;
+
+    if (timeLeft == 0) {
+      clearInterval(timer);
+      gameOver();
+    }
+  }, 1000);
+
+}
+
+/* affiche la fenetre de jeu et démarre le compteur ( fonction précédente ) */
+function gameLauncher() {
+  start_window.classList.toggle('is-open');
+  counterStart(timeMax);
+}
+
+/* fonction qui lance la fenettre du game over (résultant de la fonction counterStart si timeMax arrive à zéro) */
+function gameOver() {
+  game_over_window.classList.toggle('is-open');
+}
+
+/* lance le jeu */
+start_btn.addEventListener('click', gameLauncher);
+
+restart_btn.addEventListener('click', function() {
+  game_over_window.classList.remove('is-open');
+  counterStart(timeMax);
+});
+
 
 // écouter les clicks sur les imgs
 
@@ -40,3 +89,12 @@ start_btn.addEventListener('click', generateFlags());
   });
 });
 */
+for (var i = 0; i < el.length; i++) {
+  say(i);
+}
+
+function say(num) {
+  el[num].addEventListener('click', function() {
+    console.log('je suis là' + flags[i]);
+  })
+}
