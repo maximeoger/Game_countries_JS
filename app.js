4cc489afa164c8aa445be1bf6c9772fac6d4bbb0
LeKawa <hector.travaille@hetic.net>
2018-03-08 14:44:35 +0100

    ok ?

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
-var start_btn = document.querySelector('.start');
-var restart_btn = document.querySelector('.restart');
-var to_find = document.querySelector('.to_find');
-var start_window = document.querySelector('#game-start');
-var game_over_window = document.querySelector('#game-over');
-var counter_disp = document.querySelector('.time span');
+var start_btn = document.querySelector('button');
+var el = document.querySelectorAll('.img');
 
-/* temps d'un Round en secondes ( modifiable selon la difficultée ) */
-var timeMax = 20;
+var active_flags = [0, 1, 2, 3];
 
-/* cette fonction gère le compteur, toutes les 1000 ms (1sec) il décrémente de 1 la valeur de timeMax */
-function counterStart (value) {
-    var timeLeft = value;
-    counter_disp.innerHTML = timeLeft;
 
-    timer = setInterval (function (){
+start_btn.addEventListener('click', function() {
+  //boucle pour générer les drapeaux aléatoires (4)
+  for (var i = 0; i < 4; i++) {
+    var nb = Math.floor(Math.random() * flags.length);
 
-        timeLeft --;
-        counter_disp.innerHTML = timeLeft;
+    var drapeau = flags[nb].code.toLowerCase();
+    el[i].setAttribute('src', 'flags/' + drapeau + '.svg');
 
-        if (timeLeft == 0){
-            clearInterval(timer);
-            gameOver();
-        }  
-    }, 1000);
+    var random = Math.floor(Math.random() * active_flags.length);
+
+
+
+    var number = active_flags[random];
+    active_flags.splice(random, 1);
+    console.log(active_flags);
     
-}
 
-/* affiche la fenetre de jeu et démarre le compteur ( fonction précédente ) */
-function gameLauncher () {
-    start_window.classList.toggle('is-open');
-    counterStart(timeMax); 
-}
+    if (number === 1) {
+      var name = document.querySelector('.name_flag');
+      name.innerHTML = flags[nb].name;
 
-/* fonction qui lance la fenettre du game over (résultant de la fonction counterStart si timeMax arrive à zéro) */ 
-function gameOver () {
-    game_over_window.classList.toggle('is-open');
-}
+    }
+  }
 
-/* lance le jeu */
-start_btn.addEventListener('click', gameLauncher);
-restart_btn.addEventListener('click', function(){
-    game_over_window.classList.remove('is-open');
-    counterStart(timeMax);
+});
+
+el.forEach(function(img) {
+  img.addEventListener('click', function() {
+    console.log('patate');
+  });
 });
