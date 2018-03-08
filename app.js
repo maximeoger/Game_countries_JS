48fbc441828ca288574bfd41af1c1e3bf9ca11ac
LeKawa <hector.travaille@hetic.net>
2018-03-08 15:15:44 +0100

    presque

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-var start_btn = document.querySelector('button');
 var el = document.querySelectorAll('.img');
-
 var active_flags = [0, 1, 2, 3];
 
 
-start_btn.addEventListener('click', function() {
-  //boucle pour générer les drapeaux aléatoires (4)
+function generateFlags() {
   for (var i = 0; i < 4; i++) {
     var nb = Math.floor(Math.random() * flags.length);
 
@@ -19,7 +16,7 @@ start_btn.addEventListener('click', function() {
     var number = active_flags[random];
     active_flags.splice(random, 1);
     console.log(active_flags);
-    
+
 
     if (number === 1) {
       var name = document.querySelector('.name_flag');
@@ -28,10 +25,18 @@ start_btn.addEventListener('click', function() {
     }
   }
 
-});
+}
 
-el.forEach(function(img) {
-  img.addEventListener('click', function() {
-    console.log('patate');
+
+start_btn.addEventListener('click', generateFlags());
+
+
+// écouter les clicks sur les imgs
+
+
+/*el.forEach(function(e) {
+  this.addEventListener('click', function() {
+    console.log(e);
   });
 });
+*/
